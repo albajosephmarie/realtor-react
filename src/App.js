4b178c616ff36e4844087f4b9e31f5eae6961d88
albajosephmarie <albajosephmarie@gmail.com>
2023-11-07 11:25:07 +0800

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Offers from "./pages/Offers";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
     <>
-    <h1 className='text-2xl bg-red-400'>Hello WOrld</h1></>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/sign-in" element={<SignIn />}/>
+          <Route path="/sign-up" element={<SignUp />}/>
+          <Route path="/offers" element={<Offers />}/>
+          <Route path="/forgot-password" element={<ForgotPassword />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
